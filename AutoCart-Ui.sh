c08d8375bc5d9b49bfd746e535e925433965be2a
HardiikGit <hardiik003@gmail.com>
2025-10-21 13:13:31 +0530

    Linux.sh

--- AutoCart-Ui.sh
+++
@@ -1,3 +0,0 @@
-start cmd /k "cd FrontEnd && npm run dev"
-start cmd /k "cd BackEnd && npm run dev"
-start cmd /k "cd BackEnd && node server.js"
---
+++ Linux.sh
@@ -0,0 +1,1 @@
+(cd FrontEnd && npm run dev &) && (cd BackEnd && npm run dev &) && (cd BackEnd && node server.js)
