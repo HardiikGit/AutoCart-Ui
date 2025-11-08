5baec6a6b3143ac1872b9cd01af67022169fe07c
HardiikGit <hardiik003@gmail.com>
2025-11-08 21:18:52 +0530

    jhgshdhds

--- git.sh
+++ git.sh
@@ -1,25 +1,25 @@
-# Ask for commit message
-read -p "Enter your commit message: " msg
+ # Ask for commit message
+ read -p "Enter your commit message: " msg
 
-echo
-echo "Adding all files..."
-git add .
-sleep 1
+ echo
+ echo "Adding all files..."
+ git add .
+ sleep 1
 
-echo
-echo "Committing with message: \"$msg\""
-git commit -m "$msg"
-sleep 1
+ echo
+ echo "Committing with message: \"$msg\""
+ git commit -m "$msg"
+ sleep 1
 
-echo
-echo "Pushing to repository..."
-git push origin main
-sleep 1
+ echo
+ echo "Pushing to repository..."
+ git push origin main
+ sleep 1
 
-echo
-echo "All Done . . . . . 👍"
-sleep 1
+ echo
+ echo "All Done . . . . . 👍"
+ sleep 1
 
-echo
-clear
-exit 0
+ echo
+ clear
+ exit 0
