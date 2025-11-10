166a89f3c288ca9c0e58656193a785dd54ed4c39
HardiikGit <hardiik003@gmail.com>
2025-11-10 17:54:53 +0530

    sjndfsd

--- FrontEnd/src/components/Back/ViewLatestProducts.jsx
+++ FrontEnd/src/components/Back/ViewLatestProducts.jsx
@@ -37,7 +37,7 @@ function ViewLatestProduct() {
    const data = await res.json();
 
    if (res.ok) {
-    alert("✅ Product deleted successfully!");
+    alert("Product deleted successfully!");
     fetchProducts(); // Refresh list
    } else {
     alert(data.error || "Failed to delete product");
@@ -48,36 +48,40 @@ function ViewLatestProduct() {
  };
 
  if (loading)
-  return <p className="text-center mt-10 text-gray-600">Loading products...</p>;
+  return <p>Loading products...</p>;
 
  if (products.length === 0)
-  return <p className="text-center mt-10 text-gray-600">No latest products found.</p>;
+  return <p>No latest products found.</p>;
 
  return (
-  <div className="py-10 px-4">
-   <h1 className="text-center text-3xl font-semibold mb-8">Latest Products</h1>
+  <div className="container">
+   <h1 style={{ fontSize: '20px', marginBottom: '30px' }}>Latest Products</h1>
 
-   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+   <div className="d-flex align-items-center justify-content-start" style={{ gap: '50px' }}>
     {products.map((product) => (
      <div
       key={product._id}
       className="border rounded-2xl p-4 shadow hover:shadow-lg transition w-64 text-center"
      >
-      <div className="relative w-full h-52 overflow-hidden rounded-xl mb-4 group">
+      <div className="relative img-box" style={{ zIndex: '9' }}>
        <img
         src={`${API_GET_SINGLE_PRODUCT}/${product.mainImageId}`}
         alt={product.name}
         className="w-full h-full object-cover group-hover:opacity-0 transition"
+        style={{ height: '200px', width: '250px' }}
        />
-       <img
-        src={`${API_GET_SINGLE_PRODUCT}/${product.hoverImageId}`}
-        alt={product.name}
-        className="w-full h-full object-cover absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition"
-       />
+       <div className="hover-img" style={{ position: 'absolute', top: '0', left: '0', opacity: '0', zIndex: '9999' }}>
+        <img
+         src={`${API_GET_SINGLE_PRODUCT}/${product.hoverImageId}`}
+         alt={product.name}
+         className="w-full h-full object-cover absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition"
+         style={{ height: '200px', width: '250px' }}
+        />
+       </div>
       </div>
 
       <h2 className="text-lg font-semibold">{product.name}</h2>
-      <p className="text-gray-700 mt-1">₹{product.price}</p>
+      <p className="text-gray-700 mt-1">${product.price}</p>
 
       <button
        onClick={() => handleDelete(product._id)}
