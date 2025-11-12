d558ed0c2a70c102ce8ad32d010565a5d2e89351
HardiikGit <hardiik003@gmail.com>
2025-11-12 15:34:13 +0530

    skds

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,23 +1,23 @@
 import { Routes, Route } from 'react-router'
 import './style.css'
 import './Responsive.css'
-import LoginPage from './Pages/LoginPage';
-import MainPage from './Pages/MainPage';
-import TestPages from './Pages/TestPages';
-import { CartProvider } from './Hooks/CartContext';
-import BackEndPage from './Pages/BackEndPage';
+import LoginPage from './Pages/LoginPage'
+import MainPage from './Pages/MainPage'
+import TestPages from './Pages/TestPages'
+import { CartProvider } from './Hooks/CartContext'
+import BackEndPage from './Pages/BackEndPage'
 
 function App() {
   return (
     <CartProvider>
       <Routes>
-        <Route path="" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path='/test' element={<TestPages />} />
-        <Route path='/backend' element={<BackEndPage />} />
+        <Route path="/test" element={<TestPages />} />
+        <Route path="/backend" element={<BackEndPage />} />
       </Routes>
     </CartProvider>
   )
 }
 
-export default App;
+export default App
